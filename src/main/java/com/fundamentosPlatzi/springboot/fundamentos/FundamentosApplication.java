9945288ad3a065434b56b8195db9207276a94974
CodigoJaguar <israel.san_mendoza@hotmail.com>
2023-03-23 19:41:52 -0600

    Add Pojos

--- src/main/java/com/fundamentosPlatzi/springboot/fundamentos/FundamentosApplication.java
+++ src/main/java/com/fundamentosPlatzi/springboot/fundamentos/FundamentosApplication.java
@@ -5,6 +5,7 @@ import com.fundamentosPlatzi.springboot.fundamentos.bean.MyBeanWithDependency;
 import com.fundamentosPlatzi.springboot.fundamentos.bean.MyBeanWithProperties;
 import com.fundamentosPlatzi.springboot.fundamentos.bean.MyOwnBean;
 import com.fundamentosPlatzi.springboot.fundamentos.component.ComponentDependency;
+import com.fundamentosPlatzi.springboot.fundamentos.pojo.UserPojo;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
@@ -18,13 +19,15 @@ public class FundamentosApplication implements CommandLineRunner {
 	private MyBeanWithDependency myBeanWithDependency;
 	private MyOwnBean myOwnBean;
 	private MyBeanWithProperties myBeanWithProperties;
+	private UserPojo userPojo;
 
-	public FundamentosApplication(@Qualifier("componentTwoImplement") ComponentDependency componentDependency , MyBean myBean, MyBeanWithDependency myBeanWithDependency, MyOwnBean myOwnBean,MyBeanWithProperties myBeanWithProperties){
+	public FundamentosApplication(@Qualifier("componentTwoImplement") ComponentDependency componentDependency , MyBean myBean, MyBeanWithDependency myBeanWithDependency, MyOwnBean myOwnBean,MyBeanWithProperties myBeanWithProperties, UserPojo userPojo){
 		this.componentDependency = componentDependency;
 		this.myBean = myBean;
 		this.myBeanWithDependency = myBeanWithDependency;
 		this.myOwnBean = myOwnBean;
 		this.myBeanWithProperties = myBeanWithProperties;
+		this.userPojo = userPojo;
 	}
 	public static void main(String[] args) {
 		SpringApplication.run(FundamentosApplication.class, args);
@@ -37,5 +40,6 @@ public class FundamentosApplication implements CommandLineRunner {
 		myBeanWithDependency.printWithDependency();
 		myOwnBean.printMyMessage();
 		System.out.println(myBeanWithProperties.function());
+		System.out.println(userPojo.getEmail() + "-" + userPojo.getPassword());
 	}
 }
--- src/main/java/com/fundamentosPlatzi/springboot/fundamentos/configuration/GeneralConfiguration.java
+++ src/main/java/com/fundamentosPlatzi/springboot/fundamentos/configuration/GeneralConfiguration.java
@@ -2,11 +2,14 @@ package com.fundamentosPlatzi.springboot.fundamentos.configuration;
 
 import com.fundamentosPlatzi.springboot.fundamentos.bean.MyBeanWithProperties;
 import com.fundamentosPlatzi.springboot.fundamentos.bean.MyBeanWithPropertiesImplement;
+import com.fundamentosPlatzi.springboot.fundamentos.pojo.UserPojo;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
+@EnableConfigurationProperties(UserPojo.class)
 public class GeneralConfiguration {
     @Value("${value.name}")
     private String name;
